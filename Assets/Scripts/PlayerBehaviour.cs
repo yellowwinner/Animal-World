a64589607a42c748c5a749146f7075bd40b83a48
Raxe88 <parareda8@gmail.com>
2018-10-16 23:43:48 +0200

    Disabled some shaders, added more curvature to the world, started GUI, added template player with movement

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public GameObject ui;
+	private Canvas uiCanvas;
+	public float speed;
+
+	private bool uiStatus = false;
+
+	// Use this for initialization
+	void Start () {
+		uiCanvas = ui.GetComponent<Canvas>();
+		uiCanvas.enabled = uiStatus;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey("w")){
+			transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime, Space.World);
+		}
+		if (Input.GetKey("s"))
+        {
+			transform.Translate(Vector3.back * speed * Time.fixedDeltaTime, Space.World);
+        }
+		if (Input.GetKey("d"))
+        {
+			transform.Translate(Vector3.right * speed * Time.fixedDeltaTime, Space.World);
+        }
+		if (Input.GetKey("a"))
+        {
+			transform.Translate(Vector3.left * speed * Time.fixedDeltaTime, Space.World);
+        }
+
+		//Show and hide the UI
+		if (Input.GetKeyDown("h")){
+			uiStatus = !uiStatus;
+			uiCanvas.enabled = uiStatus;
+		}
+	}
+}
